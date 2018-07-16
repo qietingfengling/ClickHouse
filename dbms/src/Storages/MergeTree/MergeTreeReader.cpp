8c3623612a76e3e7ac13b66d1d66d52ea22d2121
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-07-16 14:15:44 +0300

    Make DatsTypeWithDictionary independent from index type.

--- dbms/src/Storages/MergeTree/MergeTreeReader.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeReader.cpp
@@ -200,6 +200,11 @@ MergeTreeReader::Stream::Stream(
             getMark(right).offset_in_compressed_file - getMark(all_mark_ranges[i].begin).offset_in_compressed_file);
     }
 
+    /// Avoid empty buffer. May happen while reading dictionary for DataTypeWithDictionary.
+    /// For example: part has single dictionary and all marks point to the same position.
+    if (max_mark_range == 0)
+        max_mark_range = max_read_buffer_size;
+
     size_t buffer_size = std::min(max_read_buffer_size, max_mark_range);
 
     /// Estimate size of the data to be read.
